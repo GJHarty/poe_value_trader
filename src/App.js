a5a0b8f925a62c938d203d0dc06e7e18da9f51fa
GJHarty <gharty@live.com>
2022-01-19 16:07:10 -0600

    setup simple test request to ensure backend connection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const runTest = () => {
+    console.log("in run test");
+    
+    axios.get('/test')
+      .then(res => {
+        console.log("success");
+      })
+      .catch(err => {
+        console.log("failure", err);
+      })
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h1>POE Value Trader</h1>
+        <button onClick={runTest}>Click to test</button>
       </header>
     </div>
   );
